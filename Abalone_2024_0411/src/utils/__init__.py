0b7e3cff5ed2baf0af50a52735b5c49b467f9d87
h-hosoda-ml <h.hosoda.ml@gmail.com>
2024-04-14 23:44:58 +0900

    add: dataの読み込みを行うクラスの作成

--- Abalone_2024_0411/src/utils/__init__.py
+++ Abalone_2024_0411/src/utils/__init__.py
@@ -1,1 +1,3 @@
 from .load_datas import *
+from .data_precesser import *
+from .visualize import *
--- Abalone_2024_0411/src/utils/load_datas.py
+++ Abalone_2024_0411/src/utils/load_datas.py
@@ -4,7 +4,7 @@ import sys
 import pandas as pd
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
-from conf import DIRS
+from conf import DIRS, CFG
 
 _all_datas = [
     os.path.join(DIRS["DATA"], "train.csv"),
@@ -14,9 +14,10 @@ _all_datas = [
 
 class DataLoader:
     @staticmethod
-    def load_data(paths: list = _all_datas) -> list[pd.DataFrame]:
+    def load_data(paths: list = _all_datas, id_col: str = CFG.ID) -> list[pd.DataFrame]:
         dataframes = []
         for path in paths:
-            dataframes.append(pd.read_csv(path))
+            dataframe = pd.read_csv(path, index_col=id_col)
+            dataframes.append(dataframe)
 
         return dataframes
